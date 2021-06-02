9e793d2766c958a92507966383f7fd12d415ea9d
Lucca Ramos Peregrino <31522493+LuccaPeregrino@users.noreply.github.com>
2021-06-01 23:47:32 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -17,41 +17,43 @@ def matrix_generator(order,interval_num,result):
 	return M
 
 def basic_mult_matrix(A,B):
-	result = matrix_generator(order,0,True)
+	result = matrix_generator(A.shape[0],0,True)
 	
-	for i in range(len(A)):
-	   for j in range(len(A)):
-		   for k in range(len(B)):
-			   result[i][j] += X[i][k] * Y[k][j]
+	
+	for i in range(A.shape[0]):
+	   for j in range(A.shape[0]):
+		   for k in range(B.shape[1]):
+			   result[i,j] += A[i,k] * B[k,j]
 			   
 	return result
 	
 def strassen_mult_matrix(x, y):
-	
-    row, col = x.shape
-    row_2, col_2 = row//2, col//2
-    a, b, c, d = x[:row_2, :col_2], x[:row_2, col_2:], x[row_2:, :col_2], x[row_2:, col_2:]
-    e, f, g, h = y[:row_2, :col_2], y[:row_2, col_2:], y[row_2:, :col_2], y[row_2:, col_2:]
   
-    if len(x) == 1:
-        return x * y
+    if (x.shape[0] <= 64):
+        return basic_mult_matrix(x,y)
 
-    p1 = strassen_mult_matrix(a, f - h)  
-    p2 = strassen_mult_matrix(a + b, h)        
-    p3 = strassen_mult_matrix(c + d, e)        
-    p4 = strassen_mult_matrix(d, g - e)        
-    p5 = strassen_mult_matrix(a + d, e + h)        
-    p6 = strassen_mult_matrix(b - d, g + h)  
-    p7 = strassen_mult_matrix(a - c, e + f)  
+    else:
+        row, col = x.shape
+        row_2, col_2 = row//2, col//2
+        a, b, c, d = x[:row_2, :col_2], x[:row_2, col_2:], x[row_2:, :col_2], x[row_2:, col_2:]
+        e, f, g, h = y[:row_2, :col_2], y[:row_2, col_2:], y[row_2:, :col_2], y[row_2:, col_2:]
+	
+        p1 = strassen_mult_matrix(a, f - h)  
+        p2 = strassen_mult_matrix(a + b, h)        
+        p3 = strassen_mult_matrix(c + d, e)        
+        p4 = strassen_mult_matrix(d, g - e)        
+        p5 = strassen_mult_matrix(a + d, e + h)        
+        p6 = strassen_mult_matrix(b - d, g + h)  
+        p7 = strassen_mult_matrix(a - c, e + f)
+		
+        c11 = p5 + p4 - p2 + p6  
+        c12 = p1 + p2           
+        c21 = p3 + p4            
+        c22 = p1 + p5 - p3 - p7  
   
-    c11 = p5 + p4 - p2 + p6  
-    c12 = p1 + p2           
-    c21 = p3 + p4            
-    c22 = p1 + p5 - p3 - p7  
-  
-    aux1 = np.hstack((c11, c12))
-    aux2 = np.hstack((c21, c22))
-    result = np.vstack((aux1,aux2))
+        aux1 = np.hstack((c11, c12))
+        aux2 = np.hstack((c21, c22))
+        result = np.vstack((aux1,aux2))
   
     return result
 
@@ -78,16 +80,16 @@ if __name__ == '__main__':
 			inicio = time.thread_time()					
 			result = basic_mult_matrix(X,Y)
 			fim = time.thread_time()
-			print(f"Naivy \n {result} \n")
+			#print(f"Naivy \n {result} \n")
 			time1_aux += (fim-inicio)
 			
 			inicio2 = time.thread_time()
 			result2 = strassen_mult_matrix(X,Y)
 			fim2 = time.thread_time()
-			print(f"Strassen \n {result2}\n")
+			#print(f"Strassen \n {result2}\n")
 			time2_aux += (fim2-inicio2)
 		
-		print(f"Ordem 2^{x+1} finalizada" )
+		#print(f"Ordem 2^{x+1} finalizada" )
 		times.append(time1_aux/int(num_matrix/2))
 		times2.append(time2_aux/int(num_matrix/2))
 		orders.append(order)
